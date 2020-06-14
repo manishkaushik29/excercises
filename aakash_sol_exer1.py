c99e9006bf7080d381014db92ee02acb98735ccd
aakashas710 <aakash.as710@gmail.com>
2020-06-14 13:05:58 +0530

    this is a exersice 1 solution file for branching

---
+++ aakash_sol_exer1.py
@@ -0,0 +1,22 @@
+def lols(s):
+    i = 0
+    j = 0
+    d = {}
+    ans = 0
+    while i < len(s):
+        if s[i] not in d or j > d[s[i]]:
+            ans = max(ans, (i - j + 1))
+            d[s[i]] = i
+        else:
+            j = d[s[i]] + 1
+            ans = max(ans, (i - j + 1))
+            i -= 1
+        i += 1
+
+    return ans
+
+
+p = lols("abcabcbb")
+q=lols("bbbbbb")
+w=lols("pwkeeee")
+print(p)
